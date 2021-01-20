96d9dc2e4f45f15e35e50a5a042ec4083043e905
Gabriel Martins <gabriel.rodrigo.mars@gmail.com>
2021-01-20 00:22:03 -0200

    Initial commit

---
+++ src/main/java/com/naescola/naescola/NaEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.naescola.naescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naescola/naescola/NaEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naescola.naescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
